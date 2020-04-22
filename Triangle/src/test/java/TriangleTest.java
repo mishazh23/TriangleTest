7b609ad1932e9aaad11b0b5871f0d539e677efca
Zhavrid Mariya <nomoresorrow23@gmail.com>
2020-04-22 19:29:36 +0300

    Last but not least

--- Triangle/src/test/java/TriangleTest.java
+++ Triangle/src/test/java/TriangleTest.java
@@ -1,5 +1,4 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 
@@ -9,22 +8,22 @@ public class TriangleTest {
 
     @Test
     public void checkTrueTestRectangle() throws Exception {
-        Assert.assertTrue(TriangleCheck.check(3, 4, 5));
+        Assertions.assertTrue(TriangleCheck.check(3, 4, 5));
     }
     @Test
     public void checkTrueTest() throws Exception {
-        Assert.assertTrue(TriangleCheck.check(5.85, 5.85, 4));
+        Assertions.assertTrue(TriangleCheck.check(5.85, 5.85, 4));
     }
     @Test
     public void checkTrueTestWithEquals() throws Exception {
-        Assert.assertTrue(TriangleCheck.check(6, 6, 6));
+        Assertions.assertTrue(TriangleCheck.check(6, 6, 6));
     }
 
 
     @Test
     public void checkFalseTest() throws Exception {
         try{
-            Assert.assertFalse(TriangleCheck.check(1, 1, 2));
+            Assertions.assertFalse(TriangleCheck.check(1, 1, 2));
         }catch (Exception e){
             Assertions.fail(e.getMessage());
         }
@@ -41,10 +40,32 @@ public class TriangleTest {
             Assertions.assertEquals(expected, actual);
         }
     }
+
+    @Test
+    public void checkFalseTestWithAllZero() throws Exception {
+        try{
+            TriangleCheck.check(0, 0, 0);
+        }catch (Exception e){
+            String expected = "Values equal or less then 0 are not allowed!";
+            String actual = e.getMessage();
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
     @Test
     public void checkFalseTestNegative() throws Exception {
         try{
             TriangleCheck.check(-3,3 , 5);
+        }catch (Exception e){
+            String expected = "Values equal or less then 0 are not allowed!";
+            String actual = e.getMessage();
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+    @Test
+    public void checkFalseTestAllNegative() throws Exception {
+        try{
+            TriangleCheck.check(-3,-4 , -5);
         }catch (Exception e){
             String expected = "Values equal or less then 0 are not allowed!";
             String actual = e.getMessage();
